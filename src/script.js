ac25ad4e136496f07b15f389ac6f48144a8df1e9
Neutoria <neutorialucas.nl@gmail.com>
2025-05-09 19:45:14 +0200

    Added the weather temperature refresher feature

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
+
+
+function searchCity (city) {
+//make the api call and update the UI
 let apiKey = "f035t400bee0c0215f1147ac3e963o6b";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
 
 
 function search (event){
@@ -6,9 +14,13 @@ function search (event){
   let userInput = document.querySelector("#search-form-input");
   let cityInput = document.querySelector("h1");
   cityInput.innerHTML = userInput.value;
+  searchCity(userInput.value);
 }
 
-
+function refreshWeather (response){
+  let temperature = document.querySelector("#weather-app-temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+}
 
 
 let searchForm = document.querySelector("#search-form");
