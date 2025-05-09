45a3744a5002df428ada099ee9c7479b075b7349
Neutoria <neutorialucas.nl@gmail.com>
2025-05-09 18:13:52 +0200

    Added axios and API key

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
+let apiKey = "f035t400bee0c0215f1147ac3e963o6b";
+
 
 function search (event){
-event.preventDefault();
-let userInput = document.querySelector("#search-form-input");
-let cityInput = document.querySelector("h1");
-cityInput.innerHTML = userInput.value;
+  event.preventDefault();
+  let userInput = document.querySelector("#search-form-input");
+  let cityInput = document.querySelector("h1");
+  cityInput.innerHTML = userInput.value;
 }
 
 
